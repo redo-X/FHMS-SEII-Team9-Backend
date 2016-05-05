973bb98bf76f3ce76c473585c6e91b43417dfe1b
David Melcher <dm070491@fh-muenster.de>
2016-05-05 09:57:20 +0200

    IEmployeeRepository interface defined Mock Implementation of IEmployeeRepository added

--- WarehouseService-shared/src/main/java/de/warehouse/shared/Employee.java
+++ WarehouseService-shared/src/main/java/de/warehouse/shared/Employee.java
@@ -25,6 +25,80 @@ public class Employee {
 		return DateUtil.getDiffYears(dateOfBirth);
 	}
 
+	
+	/**
+	 * @return the code
+	 */
+	public Integer getCode() {
+		return code;
+	}
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the mailAddress
+	 */
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	/**
+	 * @param mailAddress the mailAddress to set
+	 */
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	/**
+	 * @return the dateOfBirth
+	 */
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	/**
+	 * @param dateOfBirth the dateOfBirth to set
+	 */
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	/**
+	 * @return the role
+	 */
+	public Role getRole() {
+		return role;
+	}
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
 
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
@@ -33,6 +107,4 @@ public class Employee {
 	public String toString() {
 		return "Employee [code=" + code + ", firstName=" + firstName + ", lastName=" + lastName + "]";
 	}
-	
-	
 }
